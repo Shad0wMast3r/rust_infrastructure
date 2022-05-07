0d3d8f5326680aaf4724610b337bf197fe570d47
Andy Kukuc <andykukuc@protonmail.com>
2022-05-07 00:10:26 -0500

    EUREKA sorta

--- rust-infratucture-programming/src/lib.rs
+++ rust-infratucture-programming/src/lib.rs
@@ -1,6 +1,5 @@
 mod utils;
 
-use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
 use web_sys;
 
@@ -10,24 +9,42 @@ use web_sys;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 //Thus right here imports 'window alert'
+
+//So lets see what we can do with arrays and returning values in Rust.__rust_force_expr!
+//    /*let links2=vec!["www.google.com","www.apple.com","www.192.168.1.1"];
+    //Vectors are resizable arrays.
+    //let navigation_links:[String,3]=["www.google.com","www.apple.com","www.192.192.192"];
+
+    //Since were only using three links we only want three}
+
 #[wasm_bindgen]
 pub fn main(){
     let document=web_sys::window().unwrap().document().unwrap();
     let body=document.body().unwrap();
-
     //Math from Rust
-    let p=document.create_element("p").unwrap();
-    let x:i8=1;
+    let x:i8=1; 
     let y:i8=2;
-    let answer:i8=x+y;
+    let answer=x+y;
     let string_answer=answer.to_string();
-    p.set_inner_html(&string_answer);
-    body.append_child(&p).unwrap();
+    let p=document.create_element("p").unwrap();//initiate the element in html.
+    p.set_inner_html(&string_answer); //set the p tag in html.
+    body.append_child(&p).unwrap(); //adding the value to the P tag. WTF
+
+    //First Assign the nav container
+    let google:String="www.google.com".to_string();
+    let apple:String="www.apple.com".to_string();
+    let router:String="192.168.1.1".to_string();
+    let navigation_links=[google,apple,router];
+    let href=document.create_element("a").unwrap(); //THIS <> HTML
+    href.set_inner_html(&format!("{:?}", navigation_links.into_iter()));
+    body.append_child(&href).unwrap(); //<>
 
     //Create a canvas element
     let canvas=document.create_element("div").unwrap();
     canvas.set_id("canvas");
     body.append_child(&canvas).unwrap();
 
-    //return the value
-}
+    
+
+    
+}//end of main
